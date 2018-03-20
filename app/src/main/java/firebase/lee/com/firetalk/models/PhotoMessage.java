c79e25e251505f9a7b99678caab964905ceffabb
NOTE2-PC\Administrator <leewoo5629@gmail.com>
2018-03-20 21:35:55 +0900

    first commit

---
+++ app/src/main/java/firebase/lee/com/firetalk/models/PhotoMessage.java
@@ -0,0 +1,10 @@
+package firebase.lee.com.firetalk.models;
+
+import lombok.Data;
+
+/**
+ */
+@Data
+public class PhotoMessage extends Message {
+    private String photoUrl;
+}
---
+++ app/src/main/java/firebase/lee/com/firetalk/models/TextMessage.java
@@ -0,0 +1,8 @@
+package firebase.lee.com.firetalk.models;
+
+import lombok.Data;
+
+@Data
+public class TextMessage extends Message{
+    private String messageText;
+}
